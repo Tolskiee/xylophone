5ea822df85f2915bfc8481cba4e36b32ac45f6f6
Val Jeremy Tolosa <vjeremytolosa@gmail.com>
2022-08-19 14:56:56 +0800

    project(feature): added buttons with color and differnet sounds

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,68 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              child: Text('Play10'),
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-            ),
+          child: Column(
+            children: [
+              FlatButton(
+                child: Text(''),
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              FlatButton(
+                child: Text(''),
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              FlatButton(
+                child: Text(''),
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              FlatButton(
+                child: Text(''),
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              FlatButton(
+                child: Text(''),
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              FlatButton(
+                child: Text(''),
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              FlatButton(
+                child: Text(''),
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
